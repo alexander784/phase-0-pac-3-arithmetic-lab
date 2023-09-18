60959c6fc43312d435b9e20c0109a22395269113
alexandernyaga <alexandernyaga1999@gmail.com>
2023-09-18 13:45:23 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(a, b){return a + b}
+function subtract(a, b){return a - b}
+function multiply(a, b){return a * b}
+function divide (a, b){return a/b}
+function increment (a){return a+=1}
+function decrement (a){return a-=1}
+function makeInt(a = "0*2328"){return parseInt(a, 10)}
+function isNANmakeInt(a="sldkjflksjf"){return parseInt(a, 10)}
+function preserveDecimal(a='2.222'){return parseFloat(a,10)}
+function isNANpreserveDecimal(a='sldkjflksjf'){return parseFloat(a,10)}
